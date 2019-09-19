d530e81e1954ba1fd1951fd87491953ab6070d28
TecAbel <abelardo96.work@gmail.com>
2019-09-19 11:37:29 -0500

    remoto heroku-cloudclever

--- recursos/sql.php
+++ recursos/sql.php
@@ -1,7 +1,15 @@
 <?php
+    #remoto Cloud Server
+    
+    $host = 'bq3oadgcinqgtheapl8t-mysql.services.clever-cloud.com';
+    $usuario = 'uyhggn0wb7rpbrtb';
+    $password = 'MBbS3jkyMR00YNsgeoOK';
+    $db = 'bq3oadgcinqgtheapl8t';
+    #local
+    /*
     $host = 'localhost';
     $usuario = 'grh_god';
     $password = 'God_G*7';
-    $db = 'grh_db';
+    $db = 'grh_db';*/
     $conn = mysqli_connect($host, $usuario, $password,$db) or die("Conexión falló" . mysqli_connect_error());  
 ?>
