0a2b47b691c49db6c49207d0bda67faa6649050f
TecAbel <abelardo96.work@gmail.com>
2019-09-24 00:02:14 -0500

    bootstrap y reportes arreglados

--- recursos/reportes-gen.php
+++ recursos/reportes-gen.php
@@ -25,61 +25,7 @@
         
     }
     $filename = "GRH $nombre a $empelador $fechaarchivo .pdf" ;
-    $data = '<body style="font-family: arial;">
-    
-    <H1 style="text-align:center">Recibo de honorarios a '. $fecha .'</H1>        
-    <div style="width: 80%; margin: 0 auto; padding: 10px" >
-        <p style="text-align: justify text-justify: inter-word;">
-            Quien suscribe el presente documento <strong>'. getNombreUsuario(). ' </strong>, manifiesta haber recibido a su entera satisfacción la cantidad de <strong>$'. getTotalReporte($numEmpleador).'</strong> MXN, misma que es entregada por <strong>'. $empelador .'</strong> '.$textoEmpresa.', en efectivo, por concepto de:
-        <p>
-    </div>
-    <div style="margin-top:20px; width: 85%; margin:0 auto;">
-        <table style="margin: 0 auto; width: 100%; border-collapse:separate; border-spacing: 0px 10px;">
-            <thead style="text-align: center; color: white; background-color:#343a40; line-height:30px;">
-                <tr>
-                    <th scope="col">Fecha</th>
-                    <th scope="col">Concepto</th>
-                    <th scope="col">Detalle</th>
-                    <th scope="col">Monto</th>
-                </tr>
-            </thead>
-            <tbody style="text-align:center;border-collapse:separate;">
-                
-                '.getInfoReporte($numEmpleador).'
-                <tr>
-                    <td colspan="4"><hr width="100%" /></td>
-                </tr>
-                <tr>
-                    <th colspan="3" style="color: #28a745;">Total</th>
-                    <th style="color: #28a745;">$ '.getTotalReporte($numEmpleador).'</th>
-                </tr>
-            </tbody>
-            
-            
-        </table>
-    </div>
-    <br>
-    <br>
-    <br>
-    <br>
-    <div style="width:200px; margin: 0 auto; text-align: center;" >
-    
-        <td colspan="4"><hr width="30%" /></td>
-    
-        <br>
-        <strong>'.getNombreUsuario().'</strong>
-    </div>
-    <div  style="font-size: 10px; width: 80%; margin: 10px auto;">
-        <p style="text-align:center;">
-        <strong>Documento generado en https://grh-beta.000webhostapp.com/ </strong>   
-        <br> GRH solo genera estos reportes <strong>con los datos que el usuario haya ingresado</strong>, con el fin de ayudar en la administración de reportes de actividades de un usuario que labore por honorarios.
-        <br>
-        Por lo anterior, GRH no se hace responsable de la información que el mismo usuario haya ingresado y presente en este documento.
-    </p>
-        
-    </div>
-    <img align="center" src="../img/logocompleto.png"  width="350px">
-</body>';
+    $totalReporte = getTotalReporte($numEmpleador);
 ?>
 
 <!DOCTYPE html>
@@ -90,17 +36,20 @@
         <meta http-equiv='X-UA-Compatible' content='ie=edge'>
         <link rel="stylesheet" href="../css/normalize.css">
         <link href="https://fonts.googleapis.com/css?family=Maven+Pro|Monoton|Paytone+One|Yellowtail&display=swap" rel="stylesheet">
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <link rel="stylesheet" href="../bootstrap/bootstrap.min.css">
+        <style>
+            
+        </style>
     </head>
     <body style="font-family: arial;">
     
-        <H1 style="text-align:center">Recibo de honorarios a 24/09/19</H1>        
+        <H1 style="text-align:center">Recibo de honorarios a <?php echo $fecha?></H1>        
         <div style="width: 80%; margin: 0 auto; padding: 10px" >
             <p style="text-align: justify text-justify: inter-word;">
-                Quien suscribe el presente documento <strong>Abelardo Aqui Arroyo </strong>, manifiesta haber recibido a su entera satisfacción la cantidad de <strong>$400</strong> MXN, misma que es entregada por <strong>Alfredo Gutiérrez</strong>  , en efectivo, por concepto de:
+                Quien suscribe el presente documento <strong><?php echo $nombre ?> </strong>, manifiesta haber recibido a su entera satisfacción la cantidad de <strong>$ <?php echo $totalReporte?> </strong> MXN, misma que es entregada por <strong><?php echo $empelador ?></strong> <?php echo $textoEmpresa ?>, en efectivo, por concepto de:
             <p>
         </div>
-        <div style='margin-top:20px; width: 85%; margin:0 auto;'>
+        <div class="container">
             <table style='margin: 0 auto; width: 100%; border-spacing: 0px 10px;'>
                 <thead style="text-align: center; color: white; background-color:#343a40; line-height:30px;">
                     <tr>
@@ -113,25 +62,13 @@
                 <tbody style="text-align:center;">
                     
                     
-                    <tr  style='color:#6c757d;'>
-                        <td >11/09/19</td>
-                        <td >Servicio</td>
-                        <td >Servicio semanal</td>
-                        <td >$ 200</td>
-                    </tr>
-                
-                    <tr  style='color:#6c757d;'>
-                        <td >20/09/19</td>
-                        <td >Servicio</td>
-                        <td >Módem estaba en banda 2.4, se cambió a 5. Problemas con cobro de Google.</td>
-                        <td >$ 200</td>
-                    </tr>
+                    <?php echo  getInfoReporte($numEmpleador); ?>
                     <tr>
                         <td colspan="4"><hr width="100%" /></td>
                     </tr>
                     <tr>
                         <th colspan="3" style="color: #28a745;">Total</th>
-                        <th style="color: #28a745;">$ 400</th>
+                        <th style="color: #28a745;">$ <?php echo $totalReporte?></th>
                         <hr width="100%" />
                     </tr>
                 </tbody>
@@ -170,38 +107,15 @@
     require_once '../dompdf/lib/php-svg-lib/src/autoload.php';
     require_once '../dompdf/src/Autoloader.php';
     Dompdf\Autoloader::register();
+
+
     use Dompdf\Dompdf;
     
-    
-   
-
     $dompdf = new Dompdf();
     $dompdf->loadHtml(ob_get_clean());
     $dompdf->setPaper('letter', 'portrait');
     $dompdf->render();
-    $pdf = $dompdf->output();
+    $dompdf->stream($filename,array("Attachment" => false));
     
-    $dompdf->stream($filename);
 
-    exit;
-    /*use Dompdf\Dompdf;
-                    array("Attachment" => false)
-    // instantiate and use the dompdf class
-    $dompdf = new Dompdf();
-    $dompdf->loadHtml('hello world');
-    $dompdf->setPaper('A4', 'landscape');
-
-    // Render the HTML as PDF
-    $dompdf->render();
-
-    // Output the generated PDF to Browser
-    $dompdf->stream();*/
-
-    #usando mpdf composer 
-    /*
-    require_once '../vendor/autoload.php';
-    $mpdf = new \Mpdf\Mpdf();
-    $mpdf->WriteHTML($data);
-    #$mpdf->Output($filename, 'D'); 
-    $mpdf->Output();*/
 ?>
